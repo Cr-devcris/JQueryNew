9333c164bd210dbcc7d72ae32465fe5b012028cf
Cristiano Ribeiro <cristiano.ribeirosantos33@gmail.com>
2023-02-13 21:03:42 -0400

    ajustes no JQuery
    colocando alguns atributos.

--- mains.js
+++ mains.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $('header button').click(function(){
         $('form').slideDown();
     })
@@ -11,4 +10,17 @@ $(document).ready(function(){
 
     $('form').on('submit', function(e){
         e.preventDeFault();
+        const enderecoDaNovaImagem = $('#endNovaImg').val();
+        const novoItem = $('<li></li>');
+        console.log(enderecoDaNovaImagem);
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="ver imagem em tamanho real">
+            ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('#endNovaImg').val('')
     })
